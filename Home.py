06c2005d3d336d600f58dc3f191c2f6dfcaa602f
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-10-20 10:10:25 -0300

    1

--- Home.py
+++ Home.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 st.set_page_config(page_title='App RRHH',
                     layout='wide')
    
-st.title('Gerencia Operativa Gestión de Recursos Humanos')
+st.title('Organigrama MDHYHGC')
+
+with open("org.html", "r") as file:
+    html_code = file.read()
+components.html(html_code, width=1700, height=800, scrolling=True)
