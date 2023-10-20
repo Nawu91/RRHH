a7f7a23f72e35f4e874bdd640db453d24bb6e2bf
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-10-20 10:08:02 -0300

    1

--- Home.py
+++ Home.py
@@ -1,13 +1,6 @@
 import streamlit as st
-from streamlit_extras.app_logo import add_logo
-from PIL import Image
 
 st.set_page_config(page_title='App RRHH',
-                    layout='wide',
-                    initial_sidebar_state="auto")
-add_logo("bavos-footer.png")
+                    layout='wide')
    
 st.title('Gerencia Operativa Gestión de Recursos Humanos')
-background_image_path = ("BA.jpg")
-background_image = Image.open(background_image_path)
-st.image(background_image, use_column_width=True)
